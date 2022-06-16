8eba5d109984c224c60ae381ccbefcca09d352d6
Perfume <perfumethedev@gmail.com>
2022-06-16 13:58:25 +0630

    fixed

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.title('Uber pickups in NYC')
 
-DATE_COLUMN = 'Date'
+DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
